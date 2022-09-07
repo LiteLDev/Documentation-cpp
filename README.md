a39f425328e50248ddd9d167fc4fb0b421f2ae44
Futrime <futrime@outlook.com>
2022-09-07 16:53:06 +0800

    Adapt the license to the main repo

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# LiteLoaderSDK Plugin Development Documentation Generator
+# LiteLoaderBDS C++ Plugin Development Documentation and API Reference Generator
 
-This is the generator for LiteLoaderBDS plugin development documentation.
+This is the generator for LiteLoaderBDS C++ plugin development documentation and API reference.
 
-If you would like to preview the latest official build, please visit [LiteLoaderSDK Plugin Development Documentation](https://cpp.docs.litebds.com/en/) or [LiteLoaderSDK插件开发文档](https://cpp.docs.litebds.com/zh-Hans/)
+If you would like to preview the latest official build, please visit [LiteLoaderBDS C++ Plugin Development Documentation](https://cpp.docs.litebds.com/en/) or [LiteLoaderBDS C++插件开发文档](https://cpp.docs.litebds.com/zh-Hans/)
 
 ## Build
 
@@ -35,5 +35,11 @@ All documentation will be generated in `/docs/en/`, `/docs/zh-Hans/` and `/docs/
 ## Contribution
 
 If you would write some pages manually, please create them in `/pages/en/` or `/pages/zh-Hans/`.
+
+If you are contributing to the general documentation, please go to [the repository for general documentation](https://github.com/LiteLDev/docs).
+
+If you are contributing to the Doxygen comments in `/SDK/`, please go to [the main repository](https://github.com/LiteLDev/LiteLoaderBDS) and modify the files in `/LiteLoader/`.
+
+You should modify the texts in different languages AT THE SAME TIME.
 Please firstly fork this repository and then create a pull request!
 Doxygen will then automatically generate the documentation along with these pages.
--- preprocessor.py
+++ preprocessor.py
@@ -12,24 +12,23 @@
 """
 
 """
-    LiteLoaderSDK Documentation Preprocessor. Preprocess the LiteLoaderSDK for
-    Doxygen documentation generation.
+    LiteLoaderBDS C++ Documentation Preprocessor, a preprocessor for
+    the C++ documentation of LiteLoaderBDS.
     Copyright (C) 2022  Futrime
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the Lesser GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
+    This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
 
     You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
-    USA
+    License along with this program.  If not, see
+    <https://www.gnu.org/licenses/>.
 """
 
 
